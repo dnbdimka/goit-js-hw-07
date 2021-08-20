92d08aa0e0f00bed3021c44132cd95d6a76cc692
Dmitriy Shadurskiy <dnbdimka@gmail.com>
2021-08-20 09:48:34 +0300

    small fix

--- js/task-08.js
+++ js/task-08.js
@@ -30,9 +30,9 @@ function onCreateBtn() {
 }
 
 function randomColor() {
-    const r = Math.round(Math.random() * (255 - 1) + 1);
-    const g = Math.round(Math.random() * (255 - 1) + 1);
-    const b = Math.round(Math.random() * (255 - 1) + 1);
+    const r = Math.random() * (255 - 1) + 1;
+    const g = Math.random() * (255 - 1) + 1;
+    const b = Math.random() * (255 - 1) + 1;
     return `rgb(${r}, ${g}, ${b})`;
 }
 
